5d1d39a7a0cd763519814182ef479e70309e569f
mostafasharaby <138678777+mostafasharaby@users.noreply.github.com>
2025-05-01 14:05:01 +0300

    finish AuthService

--- src/AuthService/Auth.Infrastructure/AuthInfrastructureDependencyInjection.cs
+++ src/AuthService/Auth.Infrastructure/AuthInfrastructureDependencyInjection.cs
@@ -2,9 +2,14 @@
 using Auth.Domain.Repositories;
 using Auth.Infrastructure.Data;
 using Auth.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Auth.Infrastructure
 {
@@ -17,6 +22,13 @@ namespace Auth.Infrastructure
             services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
             services.AddScoped<IRoleRepository, RoleRepository>();
             services.AddScoped<IClaimsRepository, ClaimsRepository>();
+            services.AddAuthenticationServices(configuration);
+
+            services.AddDbContext<AuthDbContext>(option =>
+            {
+                option.UseSqlServer(configuration.GetConnectionString("AuthConnection"),
+                    sqlServerOption => sqlServerOption.EnableRetryOnFailure());
+            });
 
 
             services.AddIdentity<AppUser, IdentityRole>(options =>
@@ -37,6 +49,46 @@ namespace Auth.Infrastructure
 
 
             return services;
+        }
+        public static void AddAuthenticationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = configuration["Jwt:ValidIssuer"],
+                    ValidateAudience = true,
+                    ValidAudience = configuration["Jwt:ValidAudience"],
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Secret"]!))
+                };
+            })
+            .AddCookie()
+            .AddGoogle(options =>
+            {
+                options.ClientId = configuration["GoogleAuth:ClientId"]!;
+                options.ClientSecret = configuration["GoogleAuth:ClientSecret"]!;
+            });
+
+            services.AddAuthorization();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("MyPolicy", builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                });
+            });
         }
     }
 }
